e3924df66f04003f98981cfc512a2c2ce346894e
Abe Rudy <aberudy@gmail.com>
2017-09-10 15:18:54 -0400

    hashes is hard

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,8 +15,13 @@ def first_challenge
 
   #your code here
 
-
-  #remember to return your newly altered contacts hash!
-  contacts
+contacts.each do |person, attr| #iterate through 1st level getting into their information, :attr
+    attr.each do |attribute, value|  #iterate through the :attr (name email etc)
+        if attribute == :favorite_icecream_flavors && attr[attribute].include?("strawberry") #when we are looking at ice cream flavors, and the list includes strawberry
+            attr[attribute].delete("strawberry") #delete strawberry
+                end
+            end
+        end
+contacts  #return our hash with the updated information
 end
 
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,12 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
+  items = []
   #code your solution here!
-
-  
-
+  groceries.each do |type, item|  #iterate through groceries
+      item.each do |food| #iterate through the individual items (values)
+          items << food #store them in our items array
+          end
+      end
+  items  #return our array, with the food items stored in it
 end
